399774b0f22ea44629963594c8e96de59e418b30
kacper <50886151+KacpersDev@users.noreply.github.com>
2022-05-30 18:16:00 +0200

    Update installation-script.sh

--- installation-script.sh
+++ installation-script.sh
@@ -17,6 +17,4 @@ if [ boolean = "y" ] then
 	apt-get install screen
 	echo "Everything has been installed correctly."
 echo "Do you want me to configurate server start file?"
-elif [[ boolean = "n" ]]; then
-	echo "Qutting script"
-	cd /
+
