61e699f017e2ec99a05c505a69dd117a8985abf8
kacper <50886151+KacpersDev@users.noreply.github.com>
2022-05-30 17:48:33 +0200

    Update installation-script.sh

--- installation-script.sh
+++ installation-script.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+echo "Start with installation"?
+read decision
+echo "Installing java"
+apt-get install java
+echo "Installing Screen"
+apt-get install screen
+echo "Please enter folder name, you would like to save server in"
+read server_saver
+echo "Please choose server folder name"
+read folder_name
+
+cd /
+cd $server_saver
+mkdir $folder_name
+echo "Created..."
