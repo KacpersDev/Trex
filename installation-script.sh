5f21f468f83a004682b29eef617004d8f09a9b61
kacper <50886151+KacpersDev@users.noreply.github.com>
2022-05-30 18:03:08 +0200

    Update installation-script.sh

--- installation-script.sh
+++ installation-script.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
 
-apt-get install curl
-echo "Start with installation"?
-read decision
-echo "Installing java"
-apt-get install java
-echo "Installing Screen"
-apt-get install screen
-echo "Please enter folder name, you would like to save server in"
-read server_saver
-echo "Please choose server folder name"
-read folder_name
+echo "Do you have curl installed? (If you don't know what you are doing type n)"
+echo "Type (y for yes or n for no)"
+read curl
+if [ "$curl" = "n" ]
+	apt-get install curl
+else
+	echo "Passing curl installation, since installed."
 
+echo "Do you have java installed? (If you don't know what you are doing type n)"
+echo "Type (y for yes or n for no)"
+read java
+if [ "$java" = "n" ]
+	apt-get install java
+	echo "Java installed"
+else
+	echo "Passing java installation, since installed"
+
+echo "Do you have screen installed? (If you don't know what you are doing type n)"
+echo "Type (y for yes, n for no)"
+read screen
+if [ "$screen" = "n" ]
+	apt-get install screen
+	echo "Script has been installed"
+else
+	echo "Passing sceen installation, since installed"
+
+echo "[CONFIGURATION]"
+echo ""
+echo "Please select a folder you would like to have server installed in"
+read installation_folder
 cd /
-cd $server_saver
-mkdir $folder_name
-echo "Created..."
-
-echo "Generating start.sh file..."
-cd $folder_name
-mkdir start.sh
-echo "Start file created"
+mkdir $installation_folder
+echo ""
+echo "Done"
+echo ""
+echo "Please enter a file name for the server"
+read file_name
+cd $installation_folder
+mkdir $file_name
+echo "Process has been done"
+echo "Enjoy the server"
