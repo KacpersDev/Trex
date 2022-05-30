085cc7acf1d7c487254899c35b3d35be64e8a26d
kacper <50886151+KacpersDev@users.noreply.github.com>
2022-05-30 18:14:56 +0200

    Update installation-script.sh

--- installation-script.sh
+++ installation-script.sh
@@ -1,43 +1,22 @@
 #!/bin/bash
 
-echo "Do you have curl installed? (If you don't know what you are doing type n)"
-echo "Type (y for yes or n for no)"
-read curl
-if [ "$curl" = "n" ]
+echo "########################################"
+echo " Minecraft Server Installation Script"
+echo " 				Made by Trex           "
+echo "				Version: 0.1"
+echo "########################################"
+
+echo "Procced with the Installation? (y for yes, n for no)"
+read boolean
+if [ boolean = "y" ] then
+	echo "Installing [curl, java, screen]"
 	apt-get install curl
-else
-	echo "Passing curl installation, since installed."
-
-echo "Do you have java installed? (If you don't know what you are doing type n)"
-echo "Type (y for yes or n for no)"
-read java
-if [ "$java" = "n" ]
+	echo "Curl has been installed. Installing java"
 	apt-get install java
-	echo "Java installed"
-else
-	echo "Passing java installation, since installed"
-
-echo "Do you have screen installed? (If you don't know what you are doing type n)"
-echo "Type (y for yes, n for no)"
-read screen
-if [ "$screen" = "n" ]
+	echo "Java has been installed. Installing screen"
 	apt-get install screen
-	echo "Script has been installed"
-else
-	echo "Passing sceen installation, since installed"
-
-echo "[CONFIGURATION]"
-echo ""
-echo "Please select a folder you would like to have server installed in"
-read installation_folder
-cd /
-mkdir $installation_folder
-echo ""
-echo "Done"
-echo ""
-echo "Please enter a file name for the server"
-read file_name
-cd $installation_folder
-mkdir $file_name
-echo "Process has been done"
-echo "Enjoy the server"
+	echo "Everything has been installed correctly."
+echo "Do you want me to configurate server start file?"
+elif [ boolean = "n" ]then
+	echo "Qutting script"
+	return
